472167228261b63ff7a22f90f2fed9ffa72d392b
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2024-11-03 17:20:11 +0300

    Implement fix for flame thrower zero initialization bug.

--- UnleashedRecomp/game.cpp
+++ UnleashedRecomp/game.cpp
@@ -292,3 +292,18 @@ PPC_FUNC(sub_82BD06C8)
 }
 
 #pragma endregion
+
+#pragma region Object Bug Fixes
+
+// CObjFlame::CObjFlame
+// A field is not zero initialized,
+// causing collisions to constantly get created
+// and slow down the game.
+PPC_FUNC_IMPL(__imp__sub_82608E60);
+PPC_FUNC(sub_82608E60)
+{
+    memset(base + ctx.r3.u32, 0, 0x154);
+    __imp__sub_82608E60(ctx, base);
+}
+
+#pragma endregion
