220da348f829f1e941991d85f5722473baa10a9c
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2024-11-06 13:38:29 +0300

    Update ShaderRecomp submodule.

--- UnleashedRecompLib/shader/shader_cache.h
+++ UnleashedRecompLib/shader/shader_cache.h
@@ -2,21 +2,21 @@
 
 struct ShaderCacheEntry
 {
-    uint64_t hash;
-    uint32_t dxilOffset;
-    uint32_t dxilSize;
-    uint32_t spirvOffset;
-    uint32_t spirvSize;
+    const uint64_t hash;
+    const uint32_t dxilOffset;
+    const uint32_t dxilSize;
+    const uint32_t spirvOffset;
+    const uint32_t spirvSize;
     void* userData;
 };
 
 extern ShaderCacheEntry g_shaderCacheEntries[];
-extern size_t g_shaderCacheEntryCount;
+extern const size_t g_shaderCacheEntryCount;
 
-extern uint8_t g_compressedDxilCache[];
-extern size_t g_dxilCacheCompressedSize;
-extern size_t g_dxilCacheDecompressedSize;
+extern const uint8_t g_compressedDxilCache[];
+extern const size_t g_dxilCacheCompressedSize;
+extern const size_t g_dxilCacheDecompressedSize;
 
-extern uint8_t g_compressedSpirvCache[];
-extern size_t g_spirvCacheCompressedSize;
-extern size_t g_spirvCacheDecompressedSize;
+extern const uint8_t g_compressedSpirvCache[];
+extern const size_t g_spirvCacheCompressedSize;
+extern const size_t g_spirvCacheDecompressedSize;
