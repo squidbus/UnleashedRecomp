0e8dbe797ba3ca40456039cef9b3dee92de99729
Lorenzo Raineri <38560522+NextinMono@users.noreply.github.com>
2025-01-13 20:51:00 +0100

    Localization: Initial Italian translation

--- UnleashedRecomp/locale/locale.cpp
+++ UnleashedRecomp/locale/locale.cpp
@@ -110,6 +110,7 @@ std::unordered_map<std::string, std::unordered_map<ELanguage, std::string>> g_lo
         {
             { ELanguage::English, "INSTALLER" },
             { ELanguage::Spanish, "INSTALADOR" },
+            { ELanguage::Italian, "INSTALLATORE" },
         },
     },
     {
@@ -117,42 +118,50 @@ std::unordered_map<std::string, std::unordered_map<ELanguage, std::string>> g_lo
         {
             { ELanguage::English, "INSTALLING" },
             { ELanguage::Spanish, "INSTALANDO" },
+            { ELanguage::Italian, "INSTALLANDO" },
         }
     },
     {
         "Installer_Page_SelectLanguage",
         {
-            { ELanguage::English, "Please select a language." }
+            { ELanguage::English, "Please select a language." },
+            { ELanguage::Italian, "Seleziona una lingua." }
         }
     },
     {
         "Installer_Page_Introduction",
         {
-            { ELanguage::English, "Welcome to Unleashed Recompiled!\n\nYou'll need an Xbox 360 copy\nof Sonic Unleashed in order to proceed with the installation." }
+            { ELanguage::English, "Welcome to Unleashed Recompiled!\n\nYou'll need an Xbox 360 copy\nof Sonic Unleashed in order to proceed with the installation." },
+            { ELanguage::Italian, "Benvenuto a Unleashed Recompiled!\n\nDovrai avere una copia di\nSonic Unleashed per la Xbox 360\nper proseguire con l'installazione." }
         }
     },
     {
         "Installer_Page_SelectGameAndUpdate",
         {
-            { ELanguage::English, "Add the files for the game and its title update. You can use digital dumps or pre-extracted folders containing the necessary files." }
+            { ELanguage::English, "Add the files for the game and its title update. You can use digital dumps or pre-extracted folders containing the necessary files." },
+            { ELanguage::Italian, "Aggiungi i file del gioco e del suo aggiornamento. Puoi utilizzare una copia digitale o delle cartelle contenenti i file necessari." }
         }
     },
     {
         "Installer_Page_SelectDLC",
         {
-            { ELanguage::English, "Add the files for the DLC. You can use digital dumps or pre-extracted folders containing the necessary files." }
+            { ELanguage::English, "Add the files for the DLC. You can use digital dumps or pre-extracted folders containing the necessary files." },
+            { ELanguage::Italian, "Aggiungi i file dei DLC. Puoi utilizzare una copia digitale o cartelle contenenti i file necessari." }
         }
     },
     {
         "Installer_Page_CheckSpace",
         {
-            { ELanguage::English, "The content will be installed to the program's folder. Please confirm you have enough free space.\n\n" }
+            { ELanguage::English, "The content will be installed to the program's folder. Please confirm you have enough free space.\n\n" },
+
+            { ELanguage::Italian, "Il contenuto verrà installato nella cartella di questo programma. Assicurati di avere abbastanza spazio libero sul tuo hard disk.\n\n" }
         }
     },
     {
         "Installer_Page_Installing",
         {
-            { ELanguage::English, "Please wait while the content is being installed..." }
+            { ELanguage::English, "Please wait while the content is being installed..." },
+            { ELanguage::Italian, "Attendi mentre il contenuto viene installato... " }
         }
     },
     {
@@ -164,31 +173,36 @@ std::unordered_map<std::string, std::unordered_map<ELanguage, std::string>> g_lo
     {
         "Installer_Page_InstallFailed",
         {
-            { ELanguage::English, "Installation failed.\n\nError:\n\n" }
+            { ELanguage::English, "Installation failed.\n\nError:\n\n" },
+            { ELanguage::Italian, "Installazione fallita.\n\nErrore:\n\n" }
         }
     },
     {
         "Installer_Step_Game",
         {
-            { ELanguage::English, "GAME" }
+            { ELanguage::English, "GAME" },
+            { ELanguage::Italian, "GIOCO" }
         }
     },
     {
         "Installer_Step_Update",
         {
-            { ELanguage::English, "UPDATE" }
+            { ELanguage::English, "UPDATE" },
+            { ELanguage::Italian, "AGGIORNAMENTO" }
         }
     },
     {
         "Installer_Step_RequiredSpace",
         {
-            { ELanguage::English, "Required space: %2.2f GiB" }
+            { ELanguage::English, "Required space: %2.2f GiB" },
+            { ELanguage::Italian, "Spazio necessario: %2.2f GiB" }
         }
     },
     {
         "Installer_Step_AvailableSpace",
         {
-            { ELanguage::English, "Available space: %2.2f GiB" }
+            { ELanguage::English, "Available space: %2.2f GiB" },
+            { ELanguage::Italian, "Spazio disponibile: %2.2f GiB" }
         }
     },
     {
@@ -197,6 +211,7 @@ std::unordered_map<std::string, std::unordered_map<ELanguage, std::string>> g_lo
             { ELanguage::English, "NEXT" },
             { ELanguage::Spanish, "SIGUIENTE" },
             { ELanguage::German, "WEITER" },
+            { ELanguage::Italian, "CONTINUA" }
         }
     },
     {
@@ -205,6 +220,7 @@ std::unordered_map<std::string, std::unordered_map<ELanguage, std::string>> g_lo
             { ELanguage::English, "SKIP" },
             { ELanguage::Spanish, "SALTAR" },
             { ELanguage::German, "ÜBERSPRINGEN" },
+            { ELanguage::Italian, "SALTA" }
         }
     },
     {
@@ -213,6 +229,7 @@ std::unordered_map<std::string, std::unordered_map<ELanguage, std::string>> g_lo
             { ELanguage::English, "ADD FILES" },
             { ELanguage::Spanish, "AÑADIR ARCHIVOS" },
             { ELanguage::German, "DATEIEN HINZUFÜGEN" },
+            { ELanguage::Italian, "AGGIUNGI FILE" }
         }
     },
     {
@@ -221,48 +238,58 @@ std::unordered_map<std::string, std::unordered_map<ELanguage, std::string>> g_lo
             { ELanguage::English, "ADD FOLDER" },
             { ELanguage::Spanish, "AÑADIR CARPETA" },
             { ELanguage::German, "ORDNER HINZUFÜGEN" },
+            { ELanguage::Italian, "AGGIUNGI CARTELLA" }
         }
     },
     {
         "Installer_Message_InvalidFilesList",
         {
-            { ELanguage::English, "The following selected files are invalid:" }
+            { ELanguage::English, "The following selected files are invalid:" },
+            { ELanguage::Italian, "I seguenti file non sono validi:" }
         }
     },
     {
         "Installer_Message_InvalidFiles",
         {
-            { ELanguage::English, "Some of the files that have\nbeen provided are not valid.\n\nPlease make sure all the\nspecified files are correct\nand try again." }
+            { ELanguage::English, "Some of the files that have\nbeen provided are not valid.\n\nPlease make sure all the\nspecified files are correct\nand try again." },
+            { ELanguage::Italian, "Alcuni dei file che sono stati\nselezionati non sono validi.\n\nAssicurati che tutti\ni file sono quelli corretti\ne riprova." }
         }
     },
     {
         "Installer_Message_IncompatibleGameData",
         {
-            { ELanguage::English, "The specified game and\nupdate file are incompatible.\n\nPlease ensure the files are\nfor the same version and\nregion and try again." }
+            { ELanguage::English, "The specified game and\nupdate file are incompatible.\n\nPlease ensure the files are\nfor the same version and\nregion and try again." },
+
+            { ELanguage::Italian, "I file del gioco\ne dell'aggiornamento sono incompatibili.\n\nAssicurati che i file sono\ndella stessa versione\ne regione e riprova." }
         }
     },
     {
         "Installer_Message_DLCWarning",
         {
-            { ELanguage::English, "It is highly recommended\nthat you install all of the\nDLC, as it includes high\nquality lighting textures\nfor the base game.\n\nAre you sure you want to\nskip this step?" }
+            { ELanguage::English, "It is highly recommended\nthat you install all of the\nDLC, as it includes high\nquality lighting textures\nfor the base game.\n\nAre you sure you want to\nskip this step?" },
+            { ELanguage::Italian, "Si consiglia di installare\ntutti i DLC, poichè includono\ntexture di illuminazione di qualità migliore.\n\nSei sicuro di voler saltare?" }
         }
     },
     {
         "Installer_Message_TitleMissingDLC",
         {
-            { ELanguage::English, "This will restart the game to\nallow you to install any DLC\nthat you are missing.\n\nInstalling DLC will improve the\nlighting quality across the game.\n\nWould you like to install missing\ncontent?" }
+            { ELanguage::English, "This will restart the game to\nallow you to install any DLC\nthat you are missing.\n\nInstalling DLC will improve the\nlighting quality across the game.\n\nWould you like to install missing\ncontent?" },
+            { ELanguage::Italian, "Questa opzione riavviera il gioco\nper farti installare qualsiasi DLC\nche non hai installato.\n\nL'installazione dei DLC migliorerà la qualità\ndell'illuminazione del gioco.\n\nVuoi installare i DLC mancanti?" }
         }
     },
     {
         "Installer_Message_Title",
         {
-            { ELanguage::English, "This restarts the game to\nallow you to install any DLC\nthat you may be missing.\n\nYou are not currently\nmissing any DLC.\n\nWould you like to proceed\nanyway?" }
+            { ELanguage::English, "This restarts the game to\nallow you to install any DLC\nthat you may be missing.\n\nYou are not currently\nmissing any DLC.\n\nWould you like to proceed\nanyway?" },
+
+            { ELanguage::Italian, "Questa opzione riavviera il gioco\nper farti installare qualsiasi DLC\nche non hai installato.\n\nHai già installato tutti i DLC.\n\nVuoi procedere comunque?" }
         }
     },
     {
         "Title_Message_Quit",
         {
-            { ELanguage::English, "Are you sure you want to quit?" }
+            { ELanguage::English, "Are you sure you want to quit?" },
+            { ELanguage::Italian, "Sei sicuro di voler uscire?" }
         }
     },
     {
@@ -290,43 +317,51 @@ std::unordered_map<std::string, std::unordered_map<ELanguage, std::string>> g_lo
     {
         "Common_Yes",
         {
-            { ELanguage::English, "Yes" }
+            { ELanguage::English, "Yes" },
+
+            { ELanguage::Italian, "Sì" }
         }
     },
     {
         "Common_No",
         {
-            { ELanguage::English, "No" }
+            { ELanguage::English, "No" },
+            { ELanguage::Italian, "No" }
         }
     },
     {
         "Common_Next",
         {
-            { ELanguage::English, "Next" }
+            { ELanguage::English, "Next" },
+            { ELanguage::Italian, "Continua" }
         }
     },
     {
         "Common_Select",
         {
-            { ELanguage::English, "Select" }
+            { ELanguage::English, "Select" },
+            { ELanguage::Italian, "Seleziona" }
         }
     },
     {
         "Common_Back",
         {
-            { ELanguage::English, "Back" }
+            { ELanguage::English, "Back" },
+            { ELanguage::Italian, "Indietro" }
         }
     },
     {
         "Common_Reset",
         {
-            { ELanguage::English, "Reset" }
+            { ELanguage::English, "Reset" },
+            { ELanguage::Italian, "Ripristina" }
         }
     },
     {
         "Common_Switch",
         {
-            { ELanguage::English, "Switch" }
+            { ELanguage::English, "Switch" },
+            { ELanguage::Italian, "Cambia" }
         }
     }
 };
