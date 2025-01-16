666f93843d7118782284b36cffa12fdf6747d55f
Hyper <34012267+hyperbx@users.noreply.github.com>
2025-01-16 15:08:48 +0000

    Calculate movie aspect ratio using vertices (#84)

--- UnleashedRecomp/patches/video_patches.cpp
+++ UnleashedRecomp/patches/video_patches.cpp
@@ -8,15 +8,19 @@ using SVertexData = SWA::Sequence::Utility::CPlayMovieWrapper::CRender::SVertexD
 PPC_FUNC_IMPL(__imp__sub_82AE30D8);
 PPC_FUNC(sub_82AE30D8)
 {
-    auto movieWidth = *(be<float>*)g_memory.Translate(ctx.r4.u32 - 0x10);
-    auto movieHeight = *(be<float>*)g_memory.Translate(ctx.r4.u32 - 0x0C);
-    auto movieAspectRatio = movieWidth / movieHeight;
-    auto windowAspectRatio = (float)GameWindow::s_width / (float)GameWindow::s_height;
+    auto pViewportWidth = (be<uint32_t>*)g_memory.Translate(ctx.r4.u32 + 0x14);
+    auto pViewportHeight = (be<uint32_t>*)g_memory.Translate(ctx.r4.u32 + 0x18);
 
     auto pTopLeft = (SVertexData*)g_memory.Translate(ctx.r4.u32 + 0x6C);
     auto pTopRight = (SVertexData*)g_memory.Translate(ctx.r4.u32 + 0x6C + sizeof(SVertexData));
     auto pBottomRight = (SVertexData*)g_memory.Translate(ctx.r4.u32 + 0x6C + sizeof(SVertexData) * 2);
     auto pBottomLeft = (SVertexData*)g_memory.Translate(ctx.r4.u32 + 0x6C + sizeof(SVertexData) * 3);
+
+    auto quadWidth = std::fabs(pTopRight->X - pTopLeft->X) * ((float)*pViewportWidth / 2);
+    auto quadHeight = std::fabs(pTopLeft->Y - pBottomLeft->Y) * ((float)*pViewportHeight / 2);
+
+    auto movieAspectRatio = quadWidth / quadHeight;
+    auto windowAspectRatio = (float)GameWindow::s_width / (float)GameWindow::s_height;
 
     auto a = -1.00078f;
     auto b = 1.00139f;
